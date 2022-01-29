3c9b6b6d9bdad1c823434ce0c92928902be15e29
AGEUV <adan.gonzalez@argano.com>
2022-01-29 16:24:49 -0700

    primer commit

---
+++ app/Http/Controllers/StatusController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StatusController extends Controller
+{
+    function show(){
+        return view('index');
+    }
+
+    function create(Request $request){
+
+    }
+}
