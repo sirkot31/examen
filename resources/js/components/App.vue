e66aeb03df5f2b572a4ec479c95aa9c73908c9fe
AGEUV <adan.gonzalez@argano.com>
2022-01-30 23:33:24 -0700

    adicion de componente principal

---
+++ resources/js/components/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+       <form action="/action_page.php">
+            <label for="fname">Scan:</label><br>
+            <input type="text" id="fname" name="fname" value="John"><br>
+            <label for="lname">Plan:</label><br>
+            <input type="text" id="lname" name="lname" value="Doe"><br><br>
+             <input type="submit" value="Submit">
+      </form>
+
+    </div>
+
+</template>
---
+++ resources/js/routes.js
@@ -0,0 +1,10 @@
+const ExampleComponent= ()=>{import('./components/ExampleComponent.vue');}
+//const Graphic =()=>{import('./components/Graphic.vue');}
+
+export const routes =[
+    {
+    name:'ExampleComponent',
+    path:'/a',
+    component:ExampleComponent
+    }
+];
